82546700f6a1f5ba56790044ef9214bdcc377721
Andrew Eckerman <6eckermans2015@gmail.com>
2015-12-04 16:09:43 -0600

    Update troop.py

--- bin/troop.py
+++ bin/troop.py
@@ -9,6 +9,9 @@ def cli(username, repo):
 	'''Trooper quickly puts any folder your in up on Github.'''
 	os.system('git init')
 	os.system('git remote add origin http://github.com/%s/%s' % (username, repo))
+	os.system('git add .')
+	os.system('git commit -m "Initial commit."')
+	os.system('git push origin master')
 
 if __name__ == '__main__':
 	cli()
