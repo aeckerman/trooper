6eeeb7f44db9528676c836303f3ace3dd6772d1f
Andrew Eckerman <6eckermans2015@gmail.com>
2015-12-17 09:09:25 -0600

    Trooper commit.

---
+++ src/trooper.py
@@ -0,0 +1,34 @@
+import click
+from colorama import Fore, Style
+import sys
+import os
+
+def pop():
+	sys.stdout.write(Style.BRIGHT + '==> ' + Style.RESET_ALL)
+
+@click.command()
+@click.argument('git_username')
+@click.argument('repository_name')
+def cli(git_username, repository_name):
+	os.system('touch ~/git_out')
+	pop()
+	os.system('git init > ~/git_out')
+	print(Style.BRIGHT + Fore.GREEN + 'Initializing Git repository...' + Style.RESET_ALL + Fore.RESET)
+	pop()
+	os.system('git remote add origin https://github.com/%s/%s > ~/git_out' % (git_username, repository_name))
+	print(Style.BRIGHT + Fore.GREEN + 'Adding remote origin...' + Style.RESET_ALL + Fore.RESET)
+	pop()
+	os.system('git add . > ~/git_out')
+	print(Style.BRIGHT + Fore.GREEN + 'Adding files...' + Style.RESET_ALL + Fore.RESET)
+	pop()
+	os.system('git commit -m "Trooper commit." > ~/git_out')
+	print(Style.BRIGHT + Fore.GREEN + 'Commiting files...' + Style.RESET_ALL + Fore.RESET)
+	pop()
+	os.system('git push origin master > ~/git_out')
+	print(Style.BRIGHT + Fore.GREEN + 'Finalizing...' + Style.RESET_ALL + Fore.RESET)
+	pop()
+	os.system('rm ~/git_out')
+	print(Style.BRIGHT + Fore.GREEN + 'Cleaning up...' + Style.RESET_ALL + Fore.RESET)
+
+if __name__ == '__main__':
+	cli()
